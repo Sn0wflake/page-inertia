0829a43e339c809376abfb9a3a0a8e04b7c68821
Juris Biezais <juris.oskars@gmail.com>
2025-01-08 10:22:36 +0200

    PI | Add fixes for language and design modules

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,8 +5,27 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, h } from 'vue';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import { createI18n } from 'vue-i18n';
+
+import en from '../lang/en.json';
+import lv from '../lang/lv.json';
+import de from '../lang/de.json';
+import ru from '../lang/ru.json';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
+const savedLocale = localStorage.getItem('locale') || 'en';
+
+const i18n = createI18n({
+    locale: savedLocale,
+    fallbackLocale: 'en',
+    messages: {
+        en,
+        lv,
+        de,
+        ru,
+    },
+});
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
@@ -19,6 +38,7 @@ createInertiaApp({
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
+            .use(i18n)
             .mount(el);
     },
     progress: {
--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,10 @@ use App\Http\Controllers\ContactController;
 use App\Http\Controllers\LanguageController;
 use App\Http\Controllers\ServiceController;
 use App\Http\Controllers\AboutController;
+use App\Http\Middleware\SetLocale;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['web', \App\Http\Middleware\SetLocale::class])->group(function () {
+Route::middleware(['web', SetLocale::class])->group(function () {
     Route::get('/', [HomeController::class, 'index'])->name('home');
     Route::get('/services', [ServiceController::class, 'index'])->name('services');
     Route::get('/about', [AboutController::class, 'index'])->name('about');
